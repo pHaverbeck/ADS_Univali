38c0a17e4f3f85e9efaa54475efe3abdeb03ffda
Paulo H. Averbeck <69552119+pHaverbeck@users.noreply.github.com>
2021-03-27 12:18:49 -0300

    HOW 1 Parcial
    Inicio de desenvolvimento do código para o conversor de bases decimal-binario do HOW I

--- HOW 1/how1.c
+++ HOW 1/how1.c
@@ -4,15 +4,18 @@ int main()
     int opcao, numero;
     //Declarar restante das variaveis
 
-    printf("************************************************\n");
-    printf("Bem vindo ao Conversor de Bases,\n por favor escolha sua opção de conversão:\n");
+    printf("\n************************************************\n");
+    printf("Bem vindo ao Conversor de Bases,\npor favor escolha sua opcao de conversao:\n");
     printf("1 - Decimal para Binario\n");
-    prtinf("2 - Binario para Decimal\n")
+    printf("2 - Binario para Decimal\n");
     printf("************************************************\n");
         scanf("%d", &opcao);
-        printf("Você escolheu a opção %d\n", opcao);
-        printf("Digite o número que deseja converter:\n");
+        printf("Voce escolheu a opcao %d\n", opcao);
+        printf("Digite o numero que deseja converter:\n");
             scanf("%d", &numero);
-            printf("A conversão do número %d para a opção %d resulta em X\n", numero, opcao);
+
+            // Fazer operacoes de conversao
+            
+            printf("A conversao do numero %d para a opcao %d resulta em X\n", numero, opcao);
 return 0;
 }
