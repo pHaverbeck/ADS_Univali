1d6c652649f8406cf7e5ec763629079d808d641b
Paulo H. Averbeck <69552119+pHaverbeck@users.noreply.github.com>
2021-04-07 15:16:14 -0300

    Nao esta retornando valor

--- HOW 1/how1.c
+++ HOW 1/how1.c
@@ -2,7 +2,7 @@
 #include <math.h>
 int main()
 {
-    int opcao, numero;
+    int opcao, numDec = 0, numBin = 0, resto = 0, casaDec = 1;
     //Declarar restante das variaveis
     printf("\n************************************************\n");
     printf("Bem vindo ao Conversor de Bases,\npor favor escolha sua opcao de conversao:\n");
@@ -15,22 +15,26 @@ int main()
             //Codigo conversao DECIMAL para BINARIO
             case 1:
             printf("Voce escolheu a opcao %d\n", opcao);
-                printf("Digite o numero que deseja converter:\n");
-                scanf("%d", &numero);
-                    // Fazer operacoes de conversao
-
-            printf("A conversao do numero %d para a opcao %d resulta em X\n", numero, opcao);
+            printf("Digite o numero que deseja converter:\n");
+                scanf("%d", &numDec);
+                printf("A conversao do numero %d de Decimal para Binario resulta em %d\n", numDec, numBin);
+                    while (numDec != 0){
+                        resto = numDec % 2;
+                        numDec /= 2;
+                        numBin += resto * casaDec;
+                        casaDec *= 10;
+                    }
+                    return numBin;
             break;
 
-            // Fazer operacoes de conversao
             //Codigo conversao BINARIO para DECIMAL
             case 2:
             printf("Voce escolheu a opcao %d\n", opcao);
                 printf("Digite o numero que deseja converter:\n");
-                scanf("%d", &numero);
+                scanf("%d", &numBin);
                     // Fazer operacoes de conversao
 
-            printf("A conversao do numero %d para a opcao %d resulta em X\n", numero, opcao);
+            printf("A conversao do numero %d de Binario para Decimal resulta em %d\n", numBin, numDec);
             break;
             default:
                 printf("Por favor escolha uma opcao valida");
