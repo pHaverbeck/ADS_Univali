63d35d5e3d50642e5ea6816f0dfc0d3ef6b4d22a
Paulo H. Averbeck <69552119+pHaverbeck@users.noreply.github.com>
2021-04-07 13:50:21 -0300

    conversao dec para bin

--- HOW 1/how1.c
+++ HOW 1/how1.c
@@ -2,7 +2,7 @@
 #include <math.h>
 int main()
 {
-    int opcao, numero;
+    int opcao;
     //Declarar restante das variaveis
 
     printf("\n************************************************\n");
@@ -16,20 +16,29 @@ int main()
         switch(opcao){
             //Codigo conversao DECIMAL para BINARIO
             case 1:
+                int numDec, numBin, resto, casaDec = 1;
                 printf("Digite o numero que deseja converter:\n");
-                scanf("%d", &numero);
+                scanf("%d", &numDec);
+                    while (numDec != 0){
+                        resto = numDec%2;
+                        numDec /= 2;
+                        numBin += resto*casaDec;
+                        casaDec *= 10;
+                    }
+                    return numBin;
                     // Fazer operacoes de conversao
             
-            printf("A conversao do numero %d para a opcao %d resulta em X\n", numero, opcao);
+            printf("A conversao do numero %d para a opcao %d resulta em %d\n", numDec, opcao, numBin);
             break;
 
             //Codigo conversao BINARIO para DECIMAL
             case 2:
+                int numDec, numBin;
                 printf("Digite o numero que deseja converter:\n");
-                scanf("%d", &numero);
+                scanf("%d", &numBin);
                     // Fazer operacoes de conversao
             
-            printf("A conversao do numero %d para a opcao %d resulta em X\n", numero, opcao);
+            printf("A conversao do numero %d para a opcao %d resulta em %d\n", numBin, opcao, numDec);
             break;
             default:
                 printf("Por favor escolha uma opção válida");
