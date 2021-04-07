92d3771d75ebeeab22f3569be0a9722d9abc78db
Paulo H. Averbeck <69552119+pHaverbeck@users.noreply.github.com>
2021-04-07 14:29:22 -0300

    restart pro switch case

--- HOW 1/how1.c
+++ HOW 1/how1.c
@@ -2,46 +2,38 @@
 #include <math.h>
 int main()
 {
-    int opcao;
+    int opcao, numero;
     //Declarar restante das variaveis
-
     printf("\n************************************************\n");
     printf("Bem vindo ao Conversor de Bases,\npor favor escolha sua opcao de conversao:\n");
     printf("1 - Decimal para Binario\n");
     printf("2 - Binario para Decimal\n");
     printf("************************************************\n");
         scanf("%d", &opcao);
-        printf("Voce escolheu a opcao %d\n", opcao);
         // Inserir um Switch Case
         switch(opcao){
             //Codigo conversao DECIMAL para BINARIO
             case 1:
-                int numDec, numBin, resto, casaDec = 1;
+            printf("Voce escolheu a opcao %d\n", opcao);
                 printf("Digite o numero que deseja converter:\n");
-                scanf("%d", &numDec);
-                    while (numDec != 0){
-                        resto = numDec%2;
-                        numDec /= 2;
-                        numBin += resto*casaDec;
-                        casaDec *= 10;
-                    }
-                    return numBin;
+                scanf("%d", &numero);
                     // Fazer operacoes de conversao
-            
-            printf("A conversao do numero %d de Decimal para Binário resulta em %d\n", numDec, numBin);
+
+            printf("A conversao do numero %d para a opcao %d resulta em X\n", numero, opcao);
             break;
 
+            // Fazer operacoes de conversao
             //Codigo conversao BINARIO para DECIMAL
             case 2:
-                int numDec, numBin;
+            printf("Voce escolheu a opcao %d\n", opcao);
                 printf("Digite o numero que deseja converter:\n");
-                scanf("%d", &numBin);
+                scanf("%d", &numero);
                     // Fazer operacoes de conversao
-            
-            printf("A conversao do numero %d de Binário para Decimal resulta em %d\n", numBin, numDec);
+
+            printf("A conversao do numero %d para a opcao %d resulta em X\n", numero, opcao);
             break;
             default:
-                printf("Por favor escolha uma opção válida");
+                printf("Por favor escolha uma opcao valida");
         }
 
 return 0;
