66f8cbdaf50af58365fd5e7bf1c9232a7a14c3e1
Paulo H. Averbeck <69552119+pHaverbeck@users.noreply.github.com>
2021-04-07 15:37:15 -0300

    Decimal para Binario semi-funcional

--- HOW 1/how1.c
+++ HOW 1/how1.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <math.h>
+int decToBin (int numDec){
+    int numBin = 0;
+    int resto, casaDec = 1;
+    while (numDec != 0){
+        resto = numDec % 2;
+        numDec /= 2;
+        numBin += resto * casaDec;
+        casaDec *= 10;
+    }
+    return numBin;
+}
 int main()
 {
-    int opcao, numDec = 0, numBin = 0, resto = 0, casaDec = 1;
+    int opcao, numDec;
+    int numBin;
     //Declarar restante das variaveis
     printf("\n************************************************\n");
     printf("Bem vindo ao Conversor de Bases,\npor favor escolha sua opcao de conversao:\n");
@@ -17,24 +29,17 @@ int main()
             printf("Voce escolheu a opcao %d\n", opcao);
             printf("Digite o numero que deseja converter:\n");
                 scanf("%d", &numDec);
-                printf("A conversao do numero %d de Decimal para Binario resulta em %d\n", numDec, numBin);
-                    while (numDec != 0){
-                        resto = numDec % 2;
-                        numDec /= 2;
-                        numBin += resto * casaDec;
-                        casaDec *= 10;
-                    }
-                    return numBin;
+                printf("A conversao do numero %d de Decimal para Binario resulta em %ld\n", numDec, decToBin(numDec));
             break;
 
             //Codigo conversao BINARIO para DECIMAL
             case 2:
-            printf("Voce escolheu a opcao %d\n", opcao);
+            /* printf("Voce escolheu a opcao %d\n", opcao);
                 printf("Digite o numero que deseja converter:\n");
                 scanf("%d", &numBin);
                     // Fazer operacoes de conversao
 
-            printf("A conversao do numero %d de Binario para Decimal resulta em %d\n", numBin, numDec);
+            printf("A conversao do numero %d de Binario para Decimal resulta em %d\n", numBin, numDec);*/
             break;
             default:
                 printf("Por favor escolha uma opcao valida");
